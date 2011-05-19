4f53f613fce06f1328be79fe30eea87c5a55ee6c
Brett Graham <graham@rowland.harvard.edu>
2011-05-19 14:00:05 -0400

    adding nelder-mead optimization

---
+++ python/nm.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os, sys
+
+import numpy as np
+
+import matplotlib
+if sys.platform == 'linux2': # for headless plot generation
+    matplotlib.use('Cairo')
+
+import pylab as pl
+import scipy.optimize
+
+from ht import Network
+import cfg
+
+channel = 15
+layer = 1
+NReps = 1
+
+if len(sys.argv) > 1:
+    channel = int(sys.argv[1])
+if len(sys.argv) > 2:
+    layer = int(sys.argv[2])
+if len(sys.argv) > 3:
+    N = int(sys.argv[3])
+
+outDir = '%s/%i/%i' % ('nm', layer, channel)
+try:
+    os.makedirs(outDir)
+except OSError:
+    pass
+
+n = Network(cfg.cfg)
+n.load_mat_file('../network.mat')
+
+# v = float(np.squeeze(n.run(x0.reshape((1,cfg.layerTestSizes[layer],cfg.layerTestSizes[layer])),channel,layer)))
+
+# print v
+
+def eval_func(x, *args):
+    # print np.mean(x), np.max(x), np.min(x)
+    v = -float(np.squeeze(n.run(x.reshape((1,cfg.layerTestSizes[layer],cfg.layerTestSizes[layer])),channel,layer)))
+    # print v
+    return v
+
+for I in xrange(NReps):
+    x0 = np.random.rand(1,cfg.layerTestSizes[layer],cfg.layerTestSizes[layer])
+    r = scipy.optimize.fmin(eval_func, x0, maxiter=1000000, full_output=1)
+    xOpt, maxResp, nIter, funcCalls, warnFlags = r
+    pl.imsave('%s/nm_%i_%.3f_%i.png' % (outDir, I, maxResp, nIter), xOpt.reshape(cfg.layerTestSizes[layer],cfg.layerTestSizes[layer]), cmap=pl.cm.gray)
