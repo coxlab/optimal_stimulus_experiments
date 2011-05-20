e8df14195591d5eb2907fe55bd6ba09a4593f6f1
Brett Graham <graham@rowland.harvard.edu>
2011-05-20 10:59:15 -0400

    fixing typos in run.sh

--- python/run.sh
+++ python/run.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-SCRIPT='nm.py'
+SCRIPT='anneal.py'
 
 # layer 1
-for C in {0..4}
+for C in {0..3}
 do
   # python $SCRIPT $C 1 100000 &
+  V=$(($C * 4))
+  echo "layer 1 channel $V"
+  python $SCRIPT $V 1 10000 &
+  echo "layer 1 channel $(($V+1))"
+  python $SCRIPT $(($V+1)) 1 10000 &
+  echo "layer 1 channel $(($V+2))"
+  python $SCRIPT $(($V+2)) 1 10000 &
+  echo "layer 1 channel $(($V+3))"
+  python $SCRIPT $(($V+3)) 1 10000 
+done
+
+# layer 2
+for C in {0..7}
+do
   V=$(($C * 4))
   echo "layer 2 channel $V"
   python $SCRIPT $V 2 1000000 &
   echo "layer 2 channel $(($V+1))"
-  python $SCRIPT $(($V+1)) 2 1000000 &
+  python $SCRIPT $(($V+1)) 2 1000000
   echo "layer 2 channel $(($V+2))"
   python $SCRIPT $(($V+2)) 2 1000000 &
   echo "layer 2 channel $(($V+3))"
-  python $SCRIPT $(($V+3)) 2 1000000 
-done
-
-# layer 2
-for C in {0..8}
-do
-  V=$(($C * 4))
-  echo "layer 2 channel $V"
-  python $SCRIPT $V 2 10000000 &
-  echo "layer 2 channel $(($V+1))"
-  python $SCRIPT $(($V+1)) 2 10000000
-  echo "layer 2 channel $(($V+2))"
-  python $SCRIPT $(($V+2)) 2 10000000 &
-  echo "layer 2 channel $(($V+3))"
-  python $SCRIPT $(($V+3)) 2 10000000
+  python $SCRIPT $(($V+3)) 2 1000000
 done
